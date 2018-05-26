2f72ebdcf4341c8056bede24a21338d99d82c9d5
da-matsumoto <da.mats1990@gmail.com>
2018-05-26 10:59:55 +0900

    css modules

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    mode : 'development',
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['es2015'],
+            plugins: ['transform-react-jsx'],
+          },
+        },
+        {
+          test: /\.css$/,
+          loaders: ['style-loader', 'css-loader?modules'],
+        },
+      ],
+    },
+    entry: './app',
+    output: {
+      path: __dirname,
+      filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx'],
+    },
+  };
